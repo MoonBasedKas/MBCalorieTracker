456643d56eae56feaedc1922809aaacc9ae4a416
MoonBasedKas <kassidy.maberry@gmail.com>
2025-08-26 19:59:09 -0600

    removed unnecessary this

--- mbcalorietracker/scenes/Home.cs
+++ mbcalorietracker/scenes/Home.cs
@@ -4,17 +4,17 @@ using System;
 public partial class Home : Node2D
 {
 
-	TextEdit input = null;
-	RichTextLabel output = null;
-	public override void _Ready()
-	{
-		this.input = (TextEdit)GetNode("Input");
-		this.output = (RichTextLabel)GetNode("Output");
-	}
+    TextEdit input = null;
+    RichTextLabel output = null;
+    public override void _Ready()
+    {
+        input = (TextEdit)GetNode("Input");
+        output = (RichTextLabel)GetNode("Output");
+    }
 
-	public void _on_button_pressed()
-	{
-		this.output.Text = this.input.Text;
-	}
+    public void _on_button_pressed()
+    {
+        output.Text = input.Text;
+    }
 
 }
