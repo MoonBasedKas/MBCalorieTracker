777340a2506c5d7674ce26b91f05b64e958b8fe5
MoonBasedKas <kassidy.maberry@gmail.com>
2025-09-14 15:45:43 -0600

    Version 0.1 complete

--- mbcalorietracker/scenes/Home.cs
+++ mbcalorietracker/scenes/Home.cs
@@ -5,16 +5,21 @@ public partial class Home : Node2D
 {
 
     TextEdit input = null;
+    TextEdit meal = null;
     RichTextLabel output = null;
     public override void _Ready()
     {
         input = (TextEdit)GetNode("Input");
         output = (RichTextLabel)GetNode("Output");
+        meal = (TextEdit)GetNode("meal");
         DateTime dt = DateTime.Now.Date;
         GameData.LoadGame();
-        output.Text = "Calories: " + GameData.GetCaloriesOn(dt.ToString("yyyy/MM/dd")).ToString();
+        output.Text = GameData.GetCaloriesOn(dt.ToString("yyyy/MM/dd")).ToString();
     }
 
+    /// <summary>
+    /// When the add button is pressed it'll update the calorie counter.
+    /// </summary>
     public void _on_button_pressed()
     {
         // output.Text = "Calories: " + input.Text;
@@ -24,7 +29,17 @@ public partial class Home : Node2D
         if (!int.TryParse(input.Text, out value)) return;
 
         GameData.InsertData(d, value);
-        output.Text = "Calories: " + GameData.GetCaloriesOn(d).ToString();
+        output.Text = GameData.GetCaloriesOn(d).ToString();
+    }
+
+    /// <summary>
+    /// Saves a meal
+    /// 
+    /// </summary>
+    public void _on_save_pressed()
+    {
+        meal.Text = "";
+        return;
     }
 
 }
